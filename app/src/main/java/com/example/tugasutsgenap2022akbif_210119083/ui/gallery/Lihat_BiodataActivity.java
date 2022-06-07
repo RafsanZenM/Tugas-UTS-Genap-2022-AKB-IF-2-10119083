f10bc30dd4af0b9ef9df8cf9186d308ad871b21a
RafsanZenM <zenmustafi@gmail.com>
2022-06-07 20:46:45 +0700

    revisi2

---
+++ app/src/main/java/com/example/tugasutsgenap2022akbif_210119083/ui/gallery/Lihat_BiodataActivity.java
@@ -0,0 +1,19 @@
+package com.example.tugasutsgenap2022akbif_210119083.ui.gallery;
+//nama : Rafsan Zen Mustaofa
+//nim : 10119083
+//kelas : IF-2
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.os.Bundle;
+
+public class Lihat_BiodataActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/example/tugasutsgenap2022akbif_210119083/ui/gallery/Update_BiodataActivity.java
@@ -0,0 +1,16 @@
+package com.example.tugasutsgenap2022akbif_210119083.ui.gallery;
+//nama : Rafsan Zen Mustaofa
+//nim : 10119083
+//kelas : IF-2
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Update_BiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
