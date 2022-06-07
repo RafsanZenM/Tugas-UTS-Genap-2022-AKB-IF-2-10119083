c1710a0a876f95648130c1dde8e9724f56550ac9
RafsanZenM <zenmustafi@gmail.com>
2022-06-07 20:34:44 +0700

    revisi1 UTS

---
+++ app/src/main/java/com/example/tugasutsgenap2022akbif_210119083/ui/gallery/Buat_BiodataActivity.java
@@ -0,0 +1,16 @@
+package com.example.tugasutsgenap2022akbif_210119083.ui.gallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tugasutsgenap2022akbif_210119083.R;
+
+public class Buat_BiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_biodata);
+    }
+}
